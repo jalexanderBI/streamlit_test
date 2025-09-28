1244c944defc0aef32925e51b10624452d9f69ce
jalexanderBI <jalexander.rodriguez2030@gmail.com>
2025-09-28 14:27:40 -0500

    initial commit

--- app.py
+++ app.py
@@ -1,21 +1,3 @@
-#import streamlit as st
-
-#st.header('Lanzar una moneda')
-#st.write('Esta aplicación aún no es funcional. En construcción.')
-
-
-#import streamlit as st
-
-#st.header('Lanzar una moneda')
-
-#number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
-#start_button = st.button('Ejecutar')
-
-#if start_button:
-#    st.write(f'Experimento con {number_of_trials} intentos en curso.')
-
-#st.write('Esta aplicación aún no es funcional. En construcción.')
-
 import scipy.stats
 import streamlit as st
 import time
@@ -24,7 +6,7 @@ st.header('Lanzar una moneda')
 
 chart = st.line_chart([0.5])
 
-def toss_coin(n): # función que emula el lanzamiento de una moneda
+def toss_coin(n):
 
     trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
 
@@ -47,3 +29,4 @@ start_button = st.button('Ejecutar')
 
 if start_button:
     st.write(f'Experimento con {number_of_trials} intentos en curso.')
+    mean = toss_coin(number_of_trials)
